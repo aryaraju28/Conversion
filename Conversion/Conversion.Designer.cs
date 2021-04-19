9e163509aa0dc2d0f74d25e4b335e7f9db2b2946
aryaraju28 <arya28199@gmail.com>
2021-04-19 13:44:24 +0530

    Add project files.

---
+++ Conversion/Conversion.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Conversion
+{
+    partial class Conversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtBinary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(139, 61);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(202, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(139, 212);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(202, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBinary
+            // 
+            this.txtBinary.Location = new System.Drawing.Point(139, 112);
+            this.txtBinary.Name = "txtBinary";
+            this.txtBinary.Size = new System.Drawing.Size(202, 20);
+            this.txtBinary.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Binary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hexadecimal";
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(139, 164);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(202, 20);
+            this.txtHex.TabIndex = 6;
+            // 
+            // Conversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtHex);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBinary);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Conversion";
+            this.Text = "Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtBinary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHex;
+    }
+}
+
---
+++ Conversion/Conversion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversion
+{
+    public partial class Conversion : Form
+    {
+        public Conversion()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBinary.Text = stringToBinary(txtInput.Text);
+            txtHex.Text = StringToHex(txtInput.Text);
+        }
+
+        private string StringToHex(string input)
+        {
+            string output = string.Empty;
+            char[] value = input.ToCharArray();
+            foreach(char L in value)
+            {
+                int v = Convert.ToInt32(L);
+                output +=string.Format("{0:x}", v);
+
+            }
+            return output;
+
+        }
+
+        private string stringToBinary(string data)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (char c in data.ToCharArray())
+                stringBuilder.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            return stringBuilder.ToString();
+
+        }
+    }
+}
